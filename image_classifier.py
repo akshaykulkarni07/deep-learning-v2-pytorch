2b8885bdaac663e4b1a48fcfffc7b37275434f07
Akshay <akshayk.vnit@gmail.com>
2018-12-24 11:53:33 +0530

    Added file for training network for final challenge.

--- image_classifier.py
+++ image_classifier.py
@@ -2,26 +2,26 @@
 # coding: utf-8
 
 # # Developing an AI application
-# 
-# Going forward, AI algorithms will be incorporated into more and more everyday applications. For example, you might want to include an image classifier in a smart phone app. To do this, you'd use a deep learning model trained on hundreds of thousands of images as part of the overall application architecture. A large part of software development in the future will be using these types of models as common parts of applications. 
-# 
-# In this project, you'll train an image classifier to recognize different species of flowers. You can imagine using something like this in a phone app that tells you the name of the flower your camera is looking at. In practice you'd train this classifier, then export it for use in your application. We'll be using [this dataset](http://www.robots.ox.ac.uk/~vgg/data/flowers/102/index.html) of 102 flower categories, you can see a few examples below. 
-# 
+#
+# Going forward, AI algorithms will be incorporated into more and more everyday applications. For example, you might want to include an image classifier in a smart phone app. To do this, you'd use a deep learning model trained on hundreds of thousands of images as part of the overall application architecture. A large part of software development in the future will be using these types of models as common parts of applications.
+#
+# In this project, you'll train an image classifier to recognize different species of flowers. You can imagine using something like this in a phone app that tells you the name of the flower your camera is looking at. In practice you'd train this classifier, then export it for use in your application. We'll be using [this dataset](http://www.robots.ox.ac.uk/~vgg/data/flowers/102/index.html) of 102 flower categories, you can see a few examples below.
+#
 # <img src='assets/Flowers.png' width=500px>
-# 
+#
 # The project is broken down into multiple steps:
-# 
+#
 # * Load and preprocess the image dataset
 # * Train the image classifier on your dataset
 # * Use the trained classifier to predict image content
-# 
+#
 # We'll lead you through each part which you'll implement in Python.
-# 
+#
 # When you've completed this project, you'll have an application that can be trained on any set of labeled images. Here your network will be learning about flowers and end up as a command line application. But, what you do with your new skills depends on your imagination and effort in building a dataset. For example, imagine an app where you take a picture of a car, it tells you what the make and model is, then looks up information about it. Go build your own dataset and make something new.
-# 
+#
 # First up is importing the packages you'll need. It's good practice to keep all the imports at the beginning of your code. As you work through this notebook and find you need to import a package, make sure to add the import up here.
 
-# In[17]:
+# In[ ]:
 
 
 # Imports here
@@ -33,22 +33,22 @@ import numpy as np
 
 
 # ## Load the data
-# 
+#
 # Here you'll use `torchvision` to load the data ([documentation](http://pytorch.org/docs/0.3.0/torchvision/index.html)). You can [download the data here](https://s3.amazonaws.com/content.udacity-data.com/courses/nd188/flower_data.zip). The dataset is split into two parts, training and validation. For the training, you'll want to apply transformations such as random scaling, cropping, and flipping. This will help the network generalize leading to better performance. If you use a pre-trained network, you'll also need to make sure the input data is resized to 224x224 pixels as required by the networks.
-# 
+#
 # The validation set is used to measure the model's performance on data it hasn't seen yet. For this you don't want any scaling or rotation transformations, but you'll need to resize then crop the images to the appropriate size.
-# 
+#
 # The pre-trained networks available from `torchvision` were trained on the ImageNet dataset where each color channel was normalized separately. For both sets you'll need to normalize the means and standard deviations of the images to what the network expects. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`, calculated from the ImageNet images.  These values will shift each color channel to be centered at 0 and range from -1 to 1.
 
-# In[4]:
+# In[ ]:
 
 
-data_dir = '/Users/akshaykulkarni/flower_data'
+data_dir = '/home/akshaykulkarni/flower_data'
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 
 
-# In[28]:
+# In[ ]:
 
 
 # TODO: Define your transforms for the training and validation sets
@@ -68,49 +68,49 @@ validloader = torch.utils.data.DataLoader(dataset = valid_dataset, shuffle = Tru
 
 
 # ### Label mapping
-# 
+#
 # You'll also need to load in a mapping from category label to category name. You can find this in the file `cat_to_name.json`. It's a JSON object which you can read in with the [`json` module](https://docs.python.org/2/library/json.html). This will give you a dictionary mapping the integer encoded categories to the actual names of the flowers.
 
-# In[25]:
+# In[ ]:
 
 
-import json
-
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
+# import json
+#
+# with open('cat_to_name.json', 'r') as f:
+#     cat_to_name = json.load(f)
 
 
 # # Building and training the classifier
-# 
+#
 # Now that the data is ready, it's time to build and train the classifier. As usual, you should use one of the pretrained models from `torchvision.models` to get the image features. Build and train a new feed-forward classifier using those features.
-# 
+#
 # We're going to leave this part up to you. If you want to talk through it with someone, chat with your fellow students! You can also ask questions on the forums or join the instructors in office hours.
-# 
+#
 # Refer to [the rubric](https://review.udacity.com/#!/rubrics/1663/view) for guidance on successfully completing this section. Things you'll need to do:
-# 
+#
 # * Load a [pre-trained network](http://pytorch.org/docs/master/torchvision/models.html) (If you need a starting point, the VGG networks work great and are straightforward to use)
 # * Define a new, untrained feed-forward network as a classifier, using ReLU activations and dropout
 # * Train the classifier layers using backpropagation using the pre-trained network to get the features
 # * Track the loss and accuracy on the validation set to determine the best hyperparameters
-# 
+#
 # We've left a cell open for you below, but use as many as you need. Our advice is to break the problem up into smaller parts you can run separately. Check that each part is doing what you expect, then move on to the next. You'll likely find that as you work through each part, you'll need to go back and modify your previous code. This is totally normal!
-# 
+#
 # When training make sure you're updating only the weights of the feed-forward network. You should be able to get the validation accuracy above 70% if you build everything right. Make sure to try different hyperparameters (learning rate, units in the classifier, epochs, etc) to find the best model. Save those hyperparameters to use as default values in the next part of the project.
 
-# In[31]:
+# In[ ]:
 
 
 # TODO: Build and train your network
 import torchvision.models as models
 # vgg16 = models.vgg16(pretrained = True)
-model = models.densenet121(pretrained = True)
+model = models.densenet201(pretrained = True)
 
 for param in model.parameters():
     param.requires_grad = False
 
 from collections import OrderedDict
 classifier = nn.Sequential(OrderedDict([
-    ('fc1', nn.Linear(25088, 4096)),
+    ('fc1', nn.Linear(1920, 4096)),
     ('relu1', nn.ReLU()),
     ('fc2', nn.Linear(4096, 4096)),
     ('relu2', nn.ReLU()),
@@ -128,10 +128,14 @@ model.features.denseblock4.denselayer32.requires_grad = True
 print(model)
 
 criterion = nn.NLLLoss()
-    
-import torch.optim as optim
-optimizer = optim.Adam(model.classifier.parameters(), lr = 1e-3)
 
+import torch.optim as optim
+optimizer = optim.Adam([{'params': model.classifier.parameters()},
+                        {'params': model.features.denseblock4.denselayer29.parameters()},
+                        {'params': model.features.denseblock4.denselayer30.parameters()},
+                        {'params': model.features.denseblock4.denselayer31.parameters()},
+                        {'params': model.features.denseblock4.denselayer32.parameters()},
+                       ], lr = 1e-3)
 if torch.cuda.is_available():
     model = model.cuda()
 
@@ -139,14 +143,14 @@ if torch.cuda.is_available():
 # In[ ]:
 
 
-num_epochs = 5
+num_epochs = 30
 
 valid_loss_min = np.Inf
 for i in range(1, num_epochs + 1):
     train_loss = 0
     valid_loss = 0
     model.train()
-    for batch_idx, (images, labels) in enumerate(trainloader):    
+    for batch_idx, (images, labels) in enumerate(trainloader):
         if torch.cuda.is_available():
             images, labels = images.cuda(), labels.cuda()
         output = model.forward(images)
@@ -155,7 +159,7 @@ for i in range(1, num_epochs + 1):
         loss.backward()
         optimizer.step()
         train_loss += loss.item() * images.size(0)
-        
+
     model.eval()
     for batch_idx, (images, labels) in enumerate(validloader):
         if torch.cuda.is_available():
@@ -163,11 +167,11 @@ for i in range(1, num_epochs + 1):
         output = model.forward(images)
         loss = criterion(output, labels)
         valid_loss += loss.item() * images.size(0)
-        
-    # print training/validation statistics 
+
+    # print training/validation statistics
     print('Epoch: {}/{} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
         i, num_epochs, train_loss, valid_loss))
-    
+
     # save model if validation loss has decreased
     if valid_loss <= valid_loss_min:
         print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
@@ -178,21 +182,21 @@ for i in range(1, num_epochs + 1):
 
 
 # ## Save the checkpoint
-# 
+#
 # Now that your network is trained, save the model so you can load it later for making predictions. You probably want to save other things such as the mapping of classes to indices which you get from one of the image datasets: `image_datasets['train'].class_to_idx`. You can attach this to the model as an attribute which makes inference easier later on.
-# 
+#
 # ```model.class_to_idx = image_datasets['train'].class_to_idx```
-# 
+#
 # Remember that you'll want to completely rebuild the model later so you can use it for inference. Make sure to include any information you need in the checkpoint. If you want to load the model and keep training, you'll want to save the number of epochs as well as the optimizer state, `optimizer.state_dict`. You'll likely want to use this trained model in the next part of the project, so best to save it now.
 
 # In[ ]:
 
 
-# TODO: Save the checkpoint 
+# TODO: Save the checkpoint
 
 
 # ## Loading the checkpoint
-# 
+#
 # At this point it's good to write a function that can load a checkpoint and rebuild the model. That way you can come back to this project and keep working on it without having to retrain the network.
 
 # In[ ]:
@@ -202,9 +206,9 @@ for i in range(1, num_epochs + 1):
 
 
 # # Inference for classification
-# 
-# Now you'll write a function to use a trained network for inference. That is, you'll pass an image into the network and predict the class of the flower in the image. Write a function called `predict` that takes an image and a model, then returns the top $K$ most likely classes along with the probabilities. It should look like 
-# 
+#
+# Now you'll write a function to use a trained network for inference. That is, you'll pass an image into the network and predict the class of the flower in the image. Write a function called `predict` that takes an image and a model, then returns the top $K$ most likely classes along with the probabilities. It should look like
+#
 # ```python
 # probs, classes = predict(image_path, model)
 # print(probs)
@@ -212,19 +216,19 @@ for i in range(1, num_epochs + 1):
 # > [ 0.01558163  0.01541934  0.01452626  0.01443549  0.01407339]
 # > ['70', '3', '45', '62', '55']
 # ```
-# 
-# First you'll need to handle processing the input image such that it can be used in your network. 
-# 
+#
+# First you'll need to handle processing the input image such that it can be used in your network.
+#
 # ## Image Preprocessing
-# 
-# You'll want to use `PIL` to load the image ([documentation](https://pillow.readthedocs.io/en/latest/reference/Image.html)). It's best to write a function that preprocesses the image so it can be used as input for the model. This function should process the images in the same manner used for training. 
-# 
+#
+# You'll want to use `PIL` to load the image ([documentation](https://pillow.readthedocs.io/en/latest/reference/Image.html)). It's best to write a function that preprocesses the image so it can be used as input for the model. This function should process the images in the same manner used for training.
+#
 # First, resize the images where the shortest side is 256 pixels, keeping the aspect ratio. This can be done with the [`thumbnail`](http://pillow.readthedocs.io/en/3.1.x/reference/Image.html#PIL.Image.Image.thumbnail) or [`resize`](http://pillow.readthedocs.io/en/3.1.x/reference/Image.html#PIL.Image.Image.thumbnail) methods. Then you'll need to crop out the center 224x224 portion of the image.
-# 
+#
 # Color channels of images are typically encoded as integers 0-255, but the model expected floats 0-1. You'll need to convert the values. It's easiest with a Numpy array, which you can get from a PIL image like so `np_image = np.array(pil_image)`.
-# 
-# As before, the network expects the images to be normalized in a specific way. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`. You'll want to subtract the means from each color channel, then divide by the standard deviation. 
-# 
+#
+# As before, the network expects the images to be normalized in a specific way. For the means, it's `[0.485, 0.456, 0.406]` and for the standard deviations `[0.229, 0.224, 0.225]`. You'll want to subtract the means from each color channel, then divide by the standard deviation.
+#
 # And finally, PyTorch expects the color channel to be the first dimension but it's the third dimension in the PIL image and Numpy array. You can reorder dimensions using [`ndarray.transpose`](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.ndarray.transpose.html). The color channel needs to be first and retain the order of the other two dimensions.
 
 # In[ ]:
@@ -234,7 +238,7 @@ def process_image(image):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
         returns an Numpy array
     '''
-    
+
     # TODO: Process a PIL image for use in a PyTorch model
 
 
@@ -247,32 +251,32 @@ def imshow(image, ax=None, title=None):
     """Imshow for Tensor."""
     if ax is None:
         fig, ax = plt.subplots()
-    
+
     # PyTorch tensors assume the color channel is the first dimension
     # but matplotlib assumes is the third dimension
     image = image.numpy().transpose((1, 2, 0))
-    
+
     # Undo preprocessing
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
     image = std * image + mean
-    
+
     # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
     image = np.clip(image, 0, 1)
-    
+
     ax.imshow(image)
-    
+
     return ax
 
 
 # ## Class Prediction
-# 
+#
 # Once you can get images in the correct format, it's time to write a function for making predictions with your model. A common practice is to predict the top 5 or so (usually called top-$K$) most probable classes. You'll want to calculate the class probabilities then find the $K$ largest values.
-# 
+#
 # To get the top $K$ largest values in a tensor use [`x.topk(k)`](http://pytorch.org/docs/master/torch.html#torch.topk). This method returns both the highest `k` probabilities and the indices of those probabilities corresponding to the classes. You need to convert from these indices to the actual class labels using `class_to_idx` which hopefully you added to the model or from an `ImageFolder` you used to load the data ([see here](#Save-the-checkpoint)). Make sure to invert the dictionary so you get a mapping from index to class as well.
-# 
+#
 # Again, this method should take a path to an image and a model checkpoint, then return the probabilities and classes.
-# 
+#
 # ```python
 # probs, classes = predict(image_path, model)
 # print(probs)
@@ -287,20 +291,19 @@ def imshow(image, ax=None, title=None):
 def predict(image_path, model, topk=5):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''
-    
+
     # TODO: Implement the code to predict the class from an image file
 
 
 # ## Sanity Checking
-# 
+#
 # Now that you can use a trained model for predictions, check to make sure it makes sense. Even if the validation accuracy is high, it's always good to check that there aren't obvious bugs. Use `matplotlib` to plot the probabilities for the top 5 classes as a bar graph, along with the input image. It should look like this:
-# 
+#
 # <img src='assets/inference_example.png' width=300px>
-# 
+#
 # You can convert from the class integer encoding to actual flower names with the `cat_to_name.json` file (should have been loaded earlier in the notebook). To show a PyTorch tensor as an image, use the `imshow` function defined above.
 
 # In[ ]:
 
 
 # TODO: Display an image along with the top 5 classes
-
